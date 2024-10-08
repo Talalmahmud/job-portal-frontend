700904b761eb6acbcf33a149abd28c543984e9d4
Talal Mahmud <mahmudtalal2@gmail.com>
2024-10-08 19:03:16 +0600

    initialized

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Job from "./pages/job";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Login />} path="/login" />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Home />} path="/" />
+
+          <Route element={<Job />} path="/job" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
